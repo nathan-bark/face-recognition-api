422355168dddd5992a559bc2d695c794ea6c522b
Nathan <nathanbark4@gmail.com>
2023-04-19 16:54:17 +0300

    update db parameters

--- server.js
+++ server.js
@@ -11,11 +11,12 @@ const image = require('./controllers/image')
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST,
     port: 5432,
-    user: "nathanbark",
-    password: "",
-    database: "smart-brain",
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   },
 });
 
